112cc6c0ba211df8981ae7fcbcac7b3afdb6e979
Ijiran <15732749090@163.com>
2021-06-06 23:26:43 +0800

    自动签到代码提交

---
+++ js/content-script.js
@@ -0,0 +1,22 @@
+(function() {
+	
+	setInterval(function(){
+		var demo = document.getElementById('elTaskWrap');
+		var liEle = demo.getElementsByTagName('li');
+		for(var i=0;i<liEle.length;i++){　　　　
+		    var aEle = liEle[i].getElementsByTagName('a');
+			if(aEle.length != 0){
+				aEle[0].click();
+			}
+		}
+	},1000);
+	
+	setInterval(function(){
+		
+		console.log(new Date()+"刷新");
+		
+		window.location.reload();
+		
+	},600000);
+	
+})();
