2d6831c7530d5025de72036b5e607b924b8f5502
qulinchao <15732749090@163.com>
2023-07-11 11:10:46 +0800

    fix: 更新版本

--- js/content-script.js
+++ js/content-script.js
@@ -3,7 +3,7 @@
 	setInterval(function(){
 		var demo = document.getElementById('elTaskWrap');
 		var liEle = demo.getElementsByTagName('li');
-		for(var i=0;i<liEle.length;i++){　　　　
+		for(var i=0;i<liEle.length;i++){
 		    var aEle = liEle[i].getElementsByTagName('a');
 			if(aEle.length != 0){
 				aEle[0].click();
